6d6b7fe5522cf7ac8969111e01a248500d19af21
notywq <barondimaranan@gmail.com>
2023-08-14 19:37:29 +0800

    Modified App.tsx with more Anchor specific methods of execution | Added basic_1 IDL to frontend | Added necessary eslint deps

--- app/cra-vite-migrated-frontend/src/App.tsx
+++ app/cra-vite-migrated-frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { ConnectionProvider, WalletProvider, useAnchorWallet } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl, Connection} from '@solana/web3.js';
 import React, { FC, ReactNode, useMemo } from 'react';
-
+import { Program, Provider, web3, BN, AnchorProvider} from '@project-serum/anchor'
 import './App.css';
 import '@solana/wallet-adapter-react-ui/styles.css';
+import basic_1 from './../basic_1.json';
 
 const App: FC = () => {
     return (
@@ -38,7 +39,7 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
              * instantiate its legacy wallet adapter here. Common legacy adapters can be found
              * in the npm package `@solana/wallet-adapter-wallets`.
              */
-            new UnsafeBurnerWalletAdapter(),
+            //new PhantomWalletAdapter(),
         ],
         // eslint-disable-next-line react-hooks/exhaustive-deps
         [network]
@@ -54,8 +55,27 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
 };
 
 const Content: FC = () => {
+    const wallet = useAnchorWallet();
+    function getProvider()
+    {
+        if (!wallet)
+        {
+            return null;
+        }
+    
+    /*Create the provider and return it to the caller*/
+    // Connection Local
+    const network = "http://127.0.0.1:8899";
+    const connection = new Connection(network,"processed");
+
+    if(wallet !== undefined)
+    {
+        let provider = new AnchorProvider(connection, wallet,{"preflightCommitment":"processed"});
+    }
+    }
     return (
         <div className="App">
+            <h1> Initial Setup! </h1>
             <button>Hey Fam</button>
             <WalletMultiButton />
         </div>
